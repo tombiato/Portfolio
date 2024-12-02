c9a6f8b449bd0bb69ad75a5409b6aba36d050cf5
Tom Biato <biatotom@gmail.com>
2024-12-02 16:32:38 +0100

    Add Hero component and utility functions for class name management

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-top-20 md:-left-32 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vh]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
