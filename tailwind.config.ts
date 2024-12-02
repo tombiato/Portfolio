6529edf12f9e455d4215a43cd0bff2e1454a32d3
Tom Biato <biatotom@gmail.com>
2024-12-02 15:02:53 +0100

    Update portfolio layout and styles, replacing default content with custom branding and color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#000",
+        100: "#000319",
       },
     },
   },
